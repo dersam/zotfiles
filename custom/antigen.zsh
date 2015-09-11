45a10b578ea798c7b04eada5aef8a6777d19f170
Sam Schmidt <samuel@dersam.net>
2015-09-11 13:30:05 -0700

    Add standard plugins via antigen.

--- custom/antigen.zsh
+++ custom/antigen.zsh
@@ -1,2 +1,17 @@
+source $HOME/antigen/antigen.zsh
+
+#Source standard oh-my-zsh bundles
+antigen bundle git
+antigen bundle osx
+antigen bundle web-search
+antigen bundle git-extras
+antigen bundle node
+antigen bundle npm
+antigen bundle composer
+antigen bundle sudo
+
+#additional bundles
 antigen bundle zsh-users/zsh-syntax-highlighting
+
+
 antigen apply
