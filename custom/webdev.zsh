18581a2b6757795e84c3eb505c4372602cff7c94
Sam Schmidt <samuel@dersam.net>
2015-09-22 11:57:57 -0400

    Add function for launching mobile emulators.

--- custom/webdev.zsh
+++ custom/webdev.zsh
@@ -1,2 +1,8 @@
 
 hash foo 2>/dev/null && eval "$(gulp --completion=zsh)"
+
+msim () {
+	if [[ $1 == 'ios' ]]; then
+		open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
+	fi
+}
