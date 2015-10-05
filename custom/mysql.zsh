9cffaf481965d35241024c66e1d46842ee33ee34
Sam Schmidt <samuel@dersam.net>
2015-10-05 10:40:52 -0400

    Set default dev db.

--- custom/mysql.zsh
+++ custom/mysql.zsh
@@ -1,7 +1,8 @@
 #Bindings and such to make mysql cli good?!
 
 alias mysql="mycli"
+export DEV_DB_NAME="develop"
 
 devdb () {
-	mysql -h develop.vagrant.dev -u root -D production
+	mysql -h develop.vagrant.dev -u root -D $DEV_DB_NAME
 }
