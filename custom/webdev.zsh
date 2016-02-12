52e7605a2a64eee330502543f775445bd30c0fce
Sam Schmidt <samuel@dersam.net>
2016-02-12 15:50:06 -0500

    Add elgalu/selenium environment build aliases.

--- custom/webdev.zsh
+++ custom/webdev.zsh
@@ -12,3 +12,8 @@ alias phpdebug='PHP_IDE_CONFIG="serverName=develop.vagrant.dev" /usr/bin/env php
 alias inphantom="phantomjs --webdriver=8643 --debug=true --ssl-protocol=any --ignore-ssl-errors=true"
 
 alias rx='rr vagrant "sudo /etc/init.d/nginx restart"'
+
+alias selbuild="docker run --rm --name=grid --add-host='develop.vagrant.dev:192.168.80.80' -p 4444:24444 -p 5920:25900 \
+  -v /dev/shm:/dev/shm -e VNC_PASSWORD=hola elgalu/selenium:2.51.0b"
+
+alias selvnc="open vnc://:hola@$(docker-machine ip default):5920"
