9a9416e3965c2ca092671a598ef32fad92092c63
Sam Schmidt <samuel@dersam.net>
2016-02-22 09:19:14 -0500

    Improve tmuxinator environment aliases.

--- custom/tmux.zsh
+++ custom/tmux.zsh
@@ -1,9 +1,18 @@
-devshell-restart () {
-	tmux kill-session -t devenv
-	tmuxinator start devenv
-}
-
-devshell-attach () {
-	tmux detach-client -s devenv
-	tmux a
+shmux () {
+	case $1 in
+		r)
+			;&
+		restart)
+			tmux kill-session -t devenv
+			tmuxinator start devenv
+			;;
+		a)
+			;&
+		attach)
+			tmux detach-client -s devenv
+			tmux a
+			;;
+		*)
+			echo "Unrecognized command " $1
+	esac
 }
