508945a7244d8aeeaf01ac00783c2eef9c900aee
Sam Schmidt <samuel@dersam.net>
2016-03-07 09:53:36 -0500

    Add maximum and minimum time.

--- hacks/bm.php
+++ hacks/bm.php
@@ -3,13 +3,21 @@ $cmd = "wget {$argv[1]} --no-check-certificate --delete-after -q --output-docume
 
 $avg = 0;
 $iterations = 10;
+$max = 0;
+$min = null;
 
 for ($i =0; $i < $iterations; $i++) {
+  echo '.';
   $start = microtime(true);
   shell_exec($cmd);
   $end = microtime(true);
-  $avg += ($end - $start);
+  $time = ($end - $start);
+  $avg += $time;
+  $max = $max < $time ? $time : $max;
+  $min =  $min == null || $min > $time ? $time : $min;
 }
 
-echo number_format(($avg/$iterations), 3).' seconds average time over 10 iterations.';
 echo PHP_EOL;
+echo 'Average: '.number_format(($avg/$iterations)*1000, 0).'ms'.PHP_EOL;
+echo 'Max: '.number_format($max*1000, 0).'ms'.PHP_EOL;
+echo 'Min: '.number_format($min*1000, 0).'ms'.PHP_EOL;
