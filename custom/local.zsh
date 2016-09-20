92100f42c6dfb5c9036c353e37a9ebf86c54eb04
Samuel Schmidt <samuel@dersam.net>
2016-09-20 12:11:34 -0400

    Delete more unused aliases.

--- custom/local.zsh
+++ custom/local.zsh
@@ -1,5 +1,3 @@
-alias yog="wine ~/Sqlyog/SQLyog\ Community/SQLyogCommunity.exe"
-
 export PATH=/usr/local/sbin:~/npm-global/bin:$PATH
 export EDITOR="vim"
 export DISABLE_AUTO_TITLE=true
@@ -11,53 +9,8 @@ gcj(){
 	git commit -m "WEBBUG-${id} #comment ${comment}"
 }
 
-vboot(){
-	cd ~/boxes/vagrantshell
-	vagrant up
-	rr vagrant "linus -S restart"
-	vcache stop
-	cd -
-}
-
-pulldotfiles(){
-	cd ~/.dotfiles
-	git checkout master-dane && git pull && git checkout master && git merge master-dane && git push
-}
-
-vcache(){
-	case $1 in
-		start)
-			ssh -qtt vagrant "sudo /etc/init.d/varnish start"
-			;;
-		stop)
-			ssh -qtt vagrant "sudo /etc/init.d/varnish stop"
-			;;
-		status)
-			ssh -qtt vagrant "sudo /etc/init.d/varnish status"
-			;;
-		restart)
-			ssh -qtt vagrant "sudo /etc/init.d/varnish restart"
-			;;
-		*)
-			echo "$1 is not a valid command."
-			;;
-	esac
-}
-
 shownames(){
 	git show $1 --name-only
-}
-
-##
-#Automate some things for coming in in the morning
-goodmorning() {
-	corgi
-	echo "Good morning, today is `date`"
-	pulldotfiles
-	brew update
-	brew outdated
-	brew upgrade
-	sudo npm install -g npm@latest
 }
 
 startredis() {
@@ -67,8 +20,6 @@ startredis() {
 re() {
 	tmux attach devenv
 }
-
-cdpath=(~ /redpower /redpower/vendor /redpower/vendor/linusshops)
 
 if [[ -f $HOME/.pigrc ]]; then
 	source ~/.pigrc
