982ff04e9e77d7b665ed0167030da0c7e03c2a05
Samuel Schmidt <samuel@dersam.net>
2016-09-23 10:29:52 -0400

    Alphabet.

--- custom/gister.zsh
+++ custom/gister.zsh
@@ -11,8 +11,8 @@ install_gist () {
   fi
 }
 
-# Benchmark url script
-install_gist benchmark_url https://gist.githubusercontent.com/dersam/95d13721dee1165f0c2c52856d7de40d/raw
-
 # Behatbox docker container loader
 install_gist behatbox https://gist.githubusercontent.com/dersam/70de2d7e505fa9bb32378f953d295ada/raw
+
+# Benchmark url script
+install_gist benchmark_url https://gist.githubusercontent.com/dersam/95d13721dee1165f0c2c52856d7de40d/raw
