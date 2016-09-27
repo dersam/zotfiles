f21732ed08fe1459c000bd4555b7bc9f64660596
Samuel Schmidt <samuel@dersam.net>
2016-09-27 15:43:35 -0400

    Remove osx aliases that I never use.

--- custom/environment.zsh
+++ custom/environment.zsh
@@ -66,13 +66,7 @@ export MANPATH="$(brew --prefix coreutils)/libexec/gnuman:$MANPATH"
 export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 
 # Aliases
-
-alias show='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-alias hide='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume 7'"
-alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
 #alias flushdns="dscacheutil -flushcache && killall -HUP mDNSResponder"
 alias flushdns="dscacheutil -flushcache"
 #alias update='dotfiles; sudo softwareupdate -i -a -v; brew doctor; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
