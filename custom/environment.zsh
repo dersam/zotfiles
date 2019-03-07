b200ac39f572f30725d200b76ae06883abe2fb64
Sam Schmidt <samuel@salesfloor.net>
2019-03-07 14:39:21 -0500

    Show hidden files on maxos

--- custom/environment.zsh
+++ custom/environment.zsh
@@ -87,6 +87,7 @@ case $ZSH_HOST_OS in
 	# Faster keyboard repeat rate
 	defaults write NSGlobalDomain KeyRepeat -int 1
 	defaults write NSGlobalDomain InitialKeyRepeat -int 12
+	defaults write com.apple.finder AppleShowAllFiles YES
 
 ;;
 esac
